a521009dffdd36559e253f631afe0e1f0c42915f
Shrey Chauhan <shrey.c@lfspl.com>
2024-08-29 10:46:51 +0530

    Changes

--- ControlAddins/GSTPortal/js/GST.js
+++ ControlAddins/GSTPortal/js/GST.js
@@ -3,7 +3,7 @@ const controlAddIn = $('#controlAddIn');
 $("#controlAddIn").html( `  
 
 
-  <iframe id="GSTs" src="https://www.gst.gov.in/"  
+  <iframe id="GSTs" src="https://www.icegate.gov.in/#"  
   width="100%" height="100%" frameborder="0" allowtransparency="true" marginwidth="0" marginheight="0">
   </iframe>
  
